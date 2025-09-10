b10886ec534b658555f156cb82483f811050c0f6
Santiago Ferrero <santifnob@gmail.com>
2025-09-10 00:19:31 -0300

    Fix desktop

--- composeApp/src/jvmMain/kotlin/org/example/project/Main.kt
+++ composeApp/src/jvmMain/kotlin/org/example/project/Main.kt
@@ -2,12 +2,13 @@ package org.example.project
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import screens.HomeScreen
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "KMP-Project",
     ) {
-        App()
+        HomeScreen()
     }
 }
